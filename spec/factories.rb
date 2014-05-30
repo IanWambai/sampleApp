3dbfdc45185e01b3d59cb6ca626e9a05cd5cb74d
Ian Wambai <ianwambai@gmail.com>
2014-05-30 21:38:34 +0300

    Improved the user show page and added a Gravatar

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,5 @@ Factory.define :user do |user|
 	user.name					"Fpo Bar"
 	user.email					"foobar@example.com"
 	user.password 				"foobar"
-	user.passwoerd_confirmation "foobar"
+	user.password_confirmation  "foobar"
+end
