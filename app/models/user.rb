85cf3418eb9619eb39c921739b85c8b4bc87a3e2
Ian Wambai <ianwambai@gmail.com>
2014-05-30 20:26:49 +0300

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,35 @@ class User < ActiveRecord::Base
 	validates :email, :presence => true, :format => {:with => email_regex}, :uniqueness => {:case_sensitive => false}
 	validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}
 
+	before_save :encrypted_password
+
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	class << self
+		def authenticate(email, submitted_password)
+			user = find_by_email(email)
+			return nil if user.nil?
+			return user if user.has_password?
+		end
+	end
+
+	private 
+		def encrypt_password
+			self.salt = generate_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
+
+		def generate_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
 end
---
+++ db/migrate/20140530165556_add_salt_to.rb
@@ -0,0 +1,4 @@
+class AddSaltTo < ActiveRecord::Migration
+  def change
+  end
+end
