7ad08ce74c2f7a3af08eb88853ecf78039420fd1
Ian Wambai <ianwambai@gmail.com>
2014-05-30 20:46:23 +0300

    Added a user factory and tests for the show page

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ end
 group :test do
 	gem 'rspec-rails'
 	gem 'spork-rails'
+	gem 'factory_girl_rails'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+Factory.define :user do |user|
+	user.name					"Fpo Bar"
+	user.email					"foobar@example.com"
+	user.password 				"foobar"
+	user.passwoerd_confirmation "foobar"
