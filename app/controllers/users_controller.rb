0465b4dce53a0516f3fd063b051076aa57b95019
Ian Wambai <ianwambai@gmail.com>
2014-06-03 05:03:03 +0300

    Completed sign in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
+      sign_in @user
   		redirect_to @user, :flash => { :success =>"Welcome to Westeros! You'll definitely die" }
   	else 
 	  	@title = "Sign up"
